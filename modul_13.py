2017873d9776d3949778416d5425cb0505bb9f1d
Aleksandra Komova <afa91@mail.ru>
2021-02-18 14:58:56 +0700

    task for modul 13

---
+++ modul_13.py
@@ -0,0 +1,18 @@
+ticket = int(input('Введите количество билетов: '))
+s = 0
+for i in range(1, ticket+1):
+    age = int(input('Введите возраст: '))
+    if age < 18:
+        s += 0
+    elif 25 > age >= 18:
+        s += 990
+    elif age >= 25:
+        s += 1390
+if ticket > 3:
+    s = s*0.9
+print('Сумма к оплате: ', s)
+
+
+
+
+
